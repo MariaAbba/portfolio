5c9bf80c8af2a9c4fc3596f24028d2d1317f91fb
Mary <gretchen7887@gmail.com>
2023-12-01 17:33:54 +0400

    Upd

--- src/components/btnDarkMode/BtnDarkMode.jsx
+++ src/components/btnDarkMode/BtnDarkMode.jsx
@@ -9,7 +9,6 @@ import './style.css'
 const BtnDarkMode = () => {
   const [darkMode, setDarkMode] = useLocalStorage('darkMode', detectDarkMode())
 
-
   const btnRef = useRef(null)
 
   useEffect(() => {
@@ -22,6 +21,15 @@ const BtnDarkMode = () => {
     }
   }, [darkMode])
 
+  useEffect(() => {
+    window
+      .matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener('change', (event) => {
+        const newColorScheme = event.matches ? 'dark' : 'light'
+        setDarkMode(newColorScheme)
+      });
+  }, [])
+
   const toggleDarkMode = () => {
     setDarkMode((currentValue) => {
       return currentValue === 'light' ? 'dark' : 'light'
@@ -30,8 +38,8 @@ const BtnDarkMode = () => {
 
   return (
     <button ref={btnRef} className="dark-mode-btn" onClick={toggleDarkMode}>
-      <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
       <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
+      <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
     </button>
   )
 }
