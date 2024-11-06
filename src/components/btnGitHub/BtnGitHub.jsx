0b353032da81d4e506ffa769e6ca55904a4a760d
Mary <gretchen7887@gmail.com>
2024-11-06 14:58:57 +0400

    Upd

--- src/components/btnGitHub/BtnGitHub.jsx
+++ src/components/btnGitHub/BtnGitHub.jsx
@@ -2,9 +2,9 @@ import gitHub from './gitHub-black.svg'
 
 const BtnGitHub = ({link}) => {
   return (
-    <a href={link}  target='_blank' rel='noreferrer' className="btn-outline">
+    <a href={link} target="_blank" rel="noreferrer" className="btn-outline">
       <img src="./img/icons/gitHub-black.svg" alt="" />
-      GitHub repo
+      Website Link
     </a>
   )
 }
