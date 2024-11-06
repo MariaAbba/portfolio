7d86256f6f17b986c79cc998901ed0af57a08b45
Mary <gretchen7887@gmail.com>
2024-11-06 11:52:10 +0400

    Update

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -9,12 +9,7 @@ const Contacts = () => {
             <h2 className="title-2">Location</h2>
             <p>Baku, Azerbaijan</p>
           </li>
-          <li className="content-list__item">
-            <h2 className="title-2"> WhatsApp</h2>
-            <p>
-              <a href="tel:+994557163327">+994 (55) 716-33-27</a>
-            </p>
-          </li>
+
           <li className="content-list__item">
             <h2 className="title-2">Email</h2>
             <p>
