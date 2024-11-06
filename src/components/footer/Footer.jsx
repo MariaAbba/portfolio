48bf54b920616f85e899e238d3cefc65cbd4fa06
Mary <gretchen7887@gmail.com>
2024-11-06 12:03:00 +0400

    Upd

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,20 +10,18 @@ const Footer = () => {
         <div className="footer__wrapper">
           <ul className="social">
             <li className="social__item">
-              <a href="#!">
+              <a href="https://github.com/MariaAbba">
                 <img src={github} alt="Link" />
               </a>
             </li>
             <li className="social__item">
-              <a href="#!">
+              <a href="https://www.linkedin.com/in/maria-abbasova/">
                 <img src={linkedin} alt="Link" />
               </a>
             </li>
           </ul>
           <div className="copyright">
-            <p>
-              &#169; {new Date().getFullYear()} 
-            </p>
+            <p>&#169; {new Date().getFullYear()}</p>
           </div>
         </div>
       </div>
